57aa62ef24a302658ebd01c8e0769b0d95b5712a
Brian Cho <briancho24@yahoo.com>
2017-05-24 08:22:59 -0400

    UI Update
    UI

--- src/DrumMachineUI.java
+++ src/DrumMachineUI.java
@@ -1,15 +1,24 @@
 /**
  * Created by celinaperalta on 5/17/17.
  */
-import java.awt.*;
+import javax.swing.*;
 
 public class DrumMachineUI {
-
-
-
-
-
-
-
-
+    private JPanel panel1;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
+    private JCheckBox checkBox4;
+    private JCheckBox checkBox5;
+    private JCheckBox checkBox6;
+    private JCheckBox checkBox7;
+    private JCheckBox checkBox8;
+    private JCheckBox checkBox9;
+    private JCheckBox checkBox10;
+    private JCheckBox checkBox11;
+    private JCheckBox checkBox12;
+    private JCheckBox checkBox13;
+    private JCheckBox checkBox14;
+    private JCheckBox checkBox15;
+    private JCheckBox checkBox16;
 }
